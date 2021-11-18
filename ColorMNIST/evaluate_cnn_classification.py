9dae56e9862db1b8e981ce3830b92fd3ed20ff88
Dyah Adila <adila@instgpu-02.cs.wisc.edu>
2021-11-18 17:35:16 -0600

    augment

--- ColorMNIST/evaluate_cnn_classification.py
+++ ColorMNIST/evaluate_cnn_classification.py
@@ -112,7 +112,6 @@ def fit_augment(model, train_loader):
             # Total correct predictions
             predicted = torch.max(output.data, 1)[1] 
             correct += (predicted == var_y_batch).sum()
-            #print(correct)
             if batch_idx % 200 == 0:
                 print('Epoch : {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\t Accuracy:{:.3f}%'.format(
                             epoch, 
@@ -129,18 +128,12 @@ def accuracy(output, target, topk=(1,)):
     with torch.no_grad():
         maxk = max(topk)
         batch_size = target.size(0)
-        # print(output)
         _, pred = output.topk(maxk, 1, True, True)
         pred = pred.t()
-        # print(target)
-        # print(pred)
         correct = pred.eq(target.view(1, -1).expand_as(pred))
-        # print(correct)
         res = []
         for k in topk:
             correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
-            # print(correct_k)
-            # exit()
             res.append(correct_k)
         return res
                 
@@ -156,22 +149,16 @@ def evaluate(model, test_loader):
             test_labels = Variable(test_labels.type(LongTensor))
             output = model(test_imgs)
             softmax_output = F.log_softmax(output, dim=1)
-            # print(output.cpu().numpy().shape)
             outputs.extend(torch.max(output, dim=1)[0].cpu().numpy())
-    #         predicted = torch.max(output,1)[1]
-    #         correct += (predicted == test_labels).sum()
             acc1 = accuracy(softmax_output, test_labels)
-            # print(acc1,"---------")
             top1 += acc1[0]
     print("In distribution test accuracy top1:{:.3f}% ".format( float(top1*100) / (len(test_loader)*BATCH_SIZE)))
-    # print("Test accuracy top5:{:.3f}% ".format( float(top5*100) / (len(test_loader)*BATCH_SIZE)))
     return np.asarray(outputs), test_labels
 
 def evaluate_ood(model, test_loader):
     model.eval()
     correct = 0
     top1 = 0
-    # top5 = 0
     threshold = -math.inf
     outputs = []
     with torch.no_grad():
@@ -203,7 +190,6 @@ def evaluate_ood(model, test_loader):
             # print(acc1)
             top1 += acc1[0]
     # print("Test accuracy top1:{:.3f}% ".format( float(top1*100) / (len(test_loader)*BATCH_SIZE)))
-    # print("Test accuracy top5:{:.3f}% ".format( float(top5*100) / (len(test_loader)*BATCH_SIZE)))
     return np.asarray(outputs),test_labels
 
 class ConcatDataset(torch.utils.data.Dataset):
@@ -215,6 +201,22 @@ class ConcatDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return min(len(d) for d in self.datasets)
+
+
+class RationDataset(torch.utils.data.Dataset):
+    def __init__(self, *datasets, ratio=0.5):
+        # self.datasets = datasets
+        self.ratio = ratio #
+        self.original_dataset = datasets[0]
+        self.intervened_dataset = datasets[1]
+
+    def __getitem__(self, i):
+        return tuple(d[i] for d in self.datasets)
+
+    def __len__(self):
+        return min(len(d) for d in self.datasets)
+        
+
 
 
 color_mnist_test_indist = '/nobackup/dyah_roopa/VAE_ColorMNIST_original/color_MNIST_1/test_0.25/in_dist/'
@@ -234,15 +236,18 @@ composed_transforms = transforms.Compose([
 test_set_indist = datasets.ImageFolder(color_mnist_test_indist, composed_transforms)
 test_set_ood = datasets.ImageFolder(color_mnist_test_ood, composed_transforms)
 test_set_spurious_ood = datasets.ImageFolder(color_mnist_test_spurious_ood, composed_transforms)
-# confound_test_set = datasets.ImageFolder(color_mnist_confound_test, composed_transforms)
 color_mnist_train_set = datasets.ImageFolder(color_mnist_train, composed_transforms)
 color_mnist_train_intervened_set = datasets.ImageFolder(color_mnist_train_intervened, composed_transforms)
-color_mnist_combined_set = ConcatDataset(color_mnist_train_set, color_mnist_train_intervened_set)
+
+print(color_mnist_train_set)
+print(color_mnist_train_set[:100])
+exit()
+
+color_mnist_combined_set = RationDataset(color_mnist_train_set, color_mnist_train_intervened_set)
 
 testloader_indist = torch.utils.data.DataLoader(test_set_indist, batch_size=BATCH_SIZE, shuffle=True)
 testloader_ood = torch.utils.data.DataLoader(test_set_ood, batch_size=BATCH_SIZE, shuffle=True)
 testloader_spurious_ood = torch.utils.data.DataLoader(test_set_spurious_ood, batch_size=BATCH_SIZE, shuffle=True)
-# confound_testloader = torch.utils.data.DataLoader(color_mnist_confound_test, batch_size=batch_size, shuffle=True)
 
 color_mnist_trainloader = torch.utils.data.DataLoader(
                                                         color_mnist_train_set, 
@@ -271,36 +276,28 @@ cnn_augment = cnn_augment.cuda()
 fit_augment(cnn_augment, combined_trainloader)
 
 print("baseline")
-#print("in-distribution")
 in_pred, _ = evaluate(cnn_baseline, testloader_indist)
 print("OOD")
 out_pred, _ = evaluate_ood(cnn_baseline, testloader_ood)
 utils.get_and_print_results(in_pred,out_pred,"dummy_ood","dummy_method")
-print("-----------------")
 print("SPURIOUS OOD")
 sp_out_pred, _ = evaluate_ood(cnn_baseline, testloader_spurious_ood)
 utils.get_and_print_results(in_pred,sp_out_pred,"dummy_ood","dummy_method")
-# print("spurious ood")
-# evaluate(cnn_baseline, testloader_spurious_ood)
 
 print("intervened")
 in_pred, in_actual = evaluate(cnn_intervened, testloader_indist)
+print("OOD")
 out_pred, out_actual = evaluate_ood(cnn_intervened, testloader_ood)
-# print("spurious ood")
-# evaluate(cnn_intervened, testloader_spurious_ood)
 utils.get_and_print_results(in_pred,out_pred,"dummy_ood","dummy_method")
-print("-----------------")
 print("SPURIOUS OOD")
 sp_out_pred, _ = evaluate_ood(cnn_intervened, testloader_spurious_ood)
 utils.get_and_print_results(in_pred,sp_out_pred,"dummy_ood","dummy_method")
 
 print("augment")
 in_pred, in_actual = evaluate(cnn_augment, testloader_indist)
+print("OOD")
 out_pred, out_actual = evaluate_ood(cnn_augment, testloader_ood)
-# print("spurious ood")
-# evaluate(cnn_intervened, testloader_spurious_ood)
 utils.get_and_print_results(in_pred,out_pred,"dummy_ood","dummy_method")
-print("-----------------")
 print("SPURIOUS OOD")
 sp_out_pred, _ = evaluate_ood(cnn_augment, testloader_spurious_ood)
 utils.get_and_print_results(in_pred,sp_out_pred,"dummy_ood","dummy_method")
